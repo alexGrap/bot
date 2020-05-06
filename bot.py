48a8f286a0c7e67459ea54038e580134bab435d0
alexGrap <64905728+alexGrap@users.noreply.github.com>
2020-05-06 18:37:58 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,34 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import apiai
+import json
+
+updater = Updater('1291839879:AAGNY9CxjlX4InYOvqi0uQOA_soVUiE0R6s', use_context=True)
+dispatcher = updater.dispatcher
+
+
+def start_Command(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text='Привет, я бот, с которым ты можешь немного поговорить. Я '
+                                                          'работаю на ИИ, поэтому мои ответы не всегда корректны, но '
+                                                          'меня еще тренируют! Пооговорим?')
+
+
+def text_Message(bot, update):
+    request = apiai.ApiAI('b2ac5a69d50645f581a8ef65c95ce218').text_request()
+    request.lang = 'ru'
+    request.session_id = 'BatlabAIBot'
+    request.query = update.message.text
+    response_Json = json.loads(request.getresponse().read().decode('utf-8'))
+    response = response_Json['result']['fulfillment']['speech']
+    if response:
+        bot.send_message(chat_id=update.message.chat_id, text=response)
+    else:
+        bot.send_message(chat_id=update.message.chat_id, text='Я Вас не совсем понял!')
+
+
+start_command_handler = CommandHandler('start', start_Command)
+text_message_handler = MessageHandler(Filters.text, text_Message)
+dispatcher.add_handler(start_command_handler)
+dispatcher.add_handler(text_message_handler)
+updater.start_polling(clean=True)
+updater.idle()
+updater.polling()
